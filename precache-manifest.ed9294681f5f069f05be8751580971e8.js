4e167160ca95e91d436b26d6228572223d3eff27
Nick Dresselhaus <nick@ylopo.com>
2018-12-30 14:07:04 -0800

    Updates

---
+++ precache-manifest.ed9294681f5f069f05be8751580971e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2ad7524d358a8223f0a",
+    "url": "/react-wiv/static/js/runtime~main.d2ad7524.js"
+  },
+  {
+    "revision": "4741808ca3842925fe02",
+    "url": "/react-wiv/static/js/main.4741808c.chunk.js"
+  },
+  {
+    "revision": "1cda733ab08c8a0d3022",
+    "url": "/react-wiv/static/js/1.1cda733a.chunk.js"
+  },
+  {
+    "revision": "4741808ca3842925fe02",
+    "url": "/react-wiv/static/css/main.5575558b.chunk.css"
+  },
+  {
+    "revision": "85374acf63b86e504747c92e3ba04c9a",
+    "url": "/react-wiv/index.html"
+  }
+];
